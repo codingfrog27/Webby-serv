dde498df88eb6e468a62741448d2f889b0ef1206
Michelle <m.stegeman@me.com>
2024-09-25 16:00:28 +0200

    start response class

---
+++ sources/Response/Response.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include "../../include/HttpRequest.hpp"
+
+class Response{
+	public:
+		Response() = delete;
+		Response(const Response &obj) = delete;
+		Response& operator=(const Response& obj) = delete;
+		~Response();
+
+
+	private:
+
+};
