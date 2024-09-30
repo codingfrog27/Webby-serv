3183282a71f941e013ce415645b80e7e8a8a67ec
mstegema <mstegema@student.codam.nl>
2024-09-30 18:12:36 +0200

    added support for delete & post methods
    still needs the correct data from the request in the post method, still needs to be tested

--- sources/Response/Response.cpp
+++ sources/Response/Response.cpp
@@ -1,6 +1,6 @@
 #include "Response.hpp"
 
-
+// do i need to init all values??
 Response::Response(HttpRequest* request) : _method_type(request->_method_type), _http_version(request->_http_version){
 	_status = "200 OK";
 	_headers["Content-Type"] = "text/html";
@@ -23,14 +23,24 @@ std::string	Response::generateResponse() const{
 
 /*	general set content-type function based on extension type	*/
 void	setContentType(std::string path, Response* response){
-	if (path.find(".html") != std::string::npos)
+	if (path.find(".html") != std::string::npos || path.find(".htm") != std::string::npos)
 		response->setHeaders("Content-Type", "text/html");
-	else if (path.find(".jpeg") != std::string::npos)
+	else if (path.find(".css") != std::string::npos)
+		response->setHeaders("Content-Type", "text/css");
+	else if (path.find(".js") != std::string::npos)
+		response->setHeaders("Content-Type", "text/javascript");
+	else if (path.find(".jpeg") != std::string::npos || path.find(".jpg") != std::string::npos)
 		response->setHeaders("Content-Type", "image/jpeg");
 	else if (path.find(".png") != std::string::npos)
 		response->setHeaders("Content-Type", "image/png");
 	else if (path.find(".gif") != std::string::npos)
 		response->setHeaders("Content-Type", "image/gif");
+	else if (path.find(".mp3") != std::string::npos)
+		response->setHeaders("Content-Type", "audio/mp3");
+	else if (path.find(".mp4") != std::string::npos)
+		response->setHeaders("Content-Type", "video/mp4");
+	else if (path.find(".pdf") != std::string::npos)
+		response->setHeaders("Content-Type", "application/pdf");
 	else
 		response->setHeaders("Content-Type", "text/plain");
 }
--- sources/Response/responseHandler.cpp
+++ sources/Response/responseHandler.cpp
@@ -12,13 +12,15 @@ static Response*	getMethod(HttpRequest* request, Response* response){
 		else
 			file.open(path);
 		if (file.is_open()){
+			file.seekg(0, std::ios::end);
 			size = file.tellg();
 			file.seekg(0, std::ios::beg);
-			char buffer[size];
-			if (file.read(buffer, size)){
-				response->setBody(buffer);
+			std::vector<char> buffer(size);
+			if (file.read(buffer.data(), size)){
+				response->setBody(buffer.data());
 				response->setHeaders("Content-Length", std::to_string(size));
 			}
+			// else throw 500;
 			file.close();
 		}
 	}
@@ -31,14 +33,59 @@ static Response*	getMethod(HttpRequest* request, Response* response){
 }
 
 static Response*	postMethod(HttpRequest* request, Response* response){
+	std::string path = resolveFilePath(request);
+	std::ofstream file;
+
+	// check for CGI??
+	if(getReadingMode(*response) == TEXT)
+		file.open(path, std::ios::app);
+	else
+		file.open(path);
+	if (file.is_open()){
+		file << request->getRawRequestData; //get body
+		file.close();
+		response->setStatus("201 Created");
+		response->setHeaders("Content-Type", "text/html");
+		response->setHeaders("Content-Length", "13");
+		response->setBody("201 Created");
+	}
+	else{
+		response->setStatus("500 Internal Server Error");
+		response->setHeaders("Content-Type", "text/html");
+		response->setHeaders("Content-Length", "24");
+		response->setBody("Internal Server Error");
+	}
 }
 
 static Response*	deleteMethod(HttpRequest* request, Response* response){
+	std::string path = resolveFilePath(request);
+
+	if (fileExists(path, response)){
+		if (std::remove(path.c_str()) == 0){
+			response->setStatus("200 OK");
+			response->setHeaders("Content-Type", "text/html");
+			response->setHeaders("Content-Length", "9");
+			response->setBody("200 OK");
+		}
+		else{
+			response->setStatus("500 Internal Server Error");
+			response->setHeaders("Content-Type", "text/html");
+			response->setHeaders("Content-Length", "24");
+			response->setBody("Internal Server Error");
+		}
+	}
+	else{
+		response->setStatus("404 Not Found");
+		response->setHeaders("Content-Type", "text/html");
+		response->setHeaders("Content-Length", "14");
+		response->setBody("404 Not Found");
+	}
 }
 
 void	responseHandler(HttpRequest* request)
 {
 	Response *response = new Response(request);
+	//cgi handler
 	if (request->_method_type == GET)
 		response = getMethod(request, response);
 	else if (request->_method_type == POST)
