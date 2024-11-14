53389dc9001aab090bb1c17a59017e0feb1034f1
mstegema <mstegema@student.codam.nl>
2024-11-14 16:23:31 +0100

    move resolving the filePath to main responseHandler function

--- sources/Response/responseHandler.cpp
+++ sources/Response/responseHandler.cpp
@@ -1,16 +1,15 @@
 #include "Response.hpp"
 
-static Response*	getMethod(Request* request, Response* response){
-	std::string path = resolveFilePath(request);
+static Response*	getMethod(Request* request, Response* response, std::string filePath){
 	std::ifstream file;
 	size_t size = 0;
 
-	if (fileExists(path)){
-		response->setContentType(path);
+	if (fileExists(filePath)){
+		response->setContentType(filePath);
 		if (getReadingMode(*response) == BINARY)
-			file.open(path, std::ios::binary); 
+			file.open(filePath, std::ios::binary); 
 		else
-			file.open(path);
+			file.open(filePath);
 		if (file.is_open()){
 			file.seekg(0, std::ios::end);
 			size = file.tellg();
@@ -36,15 +35,14 @@ static Response*	getMethod(Request* request, Response* response){
 	return (response);
 }
 
-static Response*	postMethod(Request* request, Response* response){
-	std::string path = resolveFilePath(request);
+static Response*	postMethod(Request* request, Response* response, std::string filePath){
 	std::ofstream file;
 
 	// check for CGI??
 	if(getReadingMode(*response) == BINARY)
-		file.open(path, std::ios::app);
+		file.open(filePath, std::ios::app);
 	else
-		file.open(path);
+		file.open(filePath);
 	if (file.is_open()){
 		// file << request->getRawRequestData; //get body
 		file.close();
@@ -55,11 +53,10 @@ static Response*	postMethod(Request* request, Response* response){
 	return response;
 }
 
-static Response*	deleteMethod(Request* request, Response* response){
-	std::string path = resolveFilePath(request);
+static Response*	deleteMethod(Request* request, Response* response, std::string filePath){
 
-	if (fileExists(path)){
-		if (std::remove(path.c_str()) == 0)
+	if (fileExists(filePath)){
+		if (std::remove(filePath.c_str()) == 0)
 			response->autoFillResponse("200 OK");
 		else
 			response->autoFillResponse("500 Internal Server Error");
@@ -69,18 +66,20 @@ static Response*	deleteMethod(Request* request, Response* response){
 	return response;
 }
 
-void	responseHandler(Request* request)
+void	responseHandler(Request* request, Config* config)
 {
 	Response *response = new Response(request);
+	std::string filePath = resolveFilePath(request, config);
 	std::string responseText;
+	
 	if (!request->getStatusCode().empty()) //if there was an error in (parsing) the request
 		response->autoFillResponse(request->getStatusCode());
 	else if (request->_method_type == GET)
-		response = getMethod(request, response);
+		response = getMethod(request, response, filePath);
 	else if (request->_method_type == POST)
-		response = postMethod(request, response);
+		response = postMethod(request, response, filePath);
 	else if (request->_method_type == DELETE)
-		response = deleteMethod(request, response);
+		response = deleteMethod(request, response, filePath);
 	else{
 		response->autoFillResponse("405 Method Not Allowed");
 		response->setHeaders("Allow", "GET, POST, DELETE");
