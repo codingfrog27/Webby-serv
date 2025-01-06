3898a5c1bb2b6e5e77006e527a743db1a1527a39
AntonioSimo <anton.simone95@gmail.com>
2025-01-06 14:27:27 +0100

    fixing conflicts

--- sources/Request/Request_reading.cpp
+++ sources/Request/Request_reading.cpp
@@ -28,7 +28,7 @@ connectStatus	Request::readRequest()
 			std::cout << "PARSING BODY" << std::endl;
 			parseBody();
 		}
-		signal(SIGPIPE, SIG_IGN);
+		signal(SIGPIPE, SIG_IGN); // Broken pipe error
 		if (isTimedOut(this->_startTime, this->_timeoutTime))
 			throw ClientErrorExcept(408, "Request Timeout");
 		if (_doneReading)
@@ -72,17 +72,10 @@ int	Request::readSocket(int size)
 	int	bytes_read = recv(_clientFD, buffer, size, MSG_DONTWAIT);
 	if (bytes_read <= 0)
 	{
-<<<<<<< HEAD
-		// if (bytes_read == 0)
-		// 	throw(ConnectionClosedExcep(_clientFD));
-		// else
-			throw (std::ios_base::failure("reading fail when reading from client socket"));
-=======
 		if (bytes_read == 0)
 			throw(ConnectionClosedExcep(_clientFD));
 		else
 			throw (std::ios_base::failure(" reading fail when reading from client socket"));
->>>>>>> 60f4fd8e4e043328a4997e06755efe22e1519348
 	}
 	_rawRequestData.insert(_rawRequestData.end(), buffer, buffer + bytes_read);
 	if (_rnrnFound)
