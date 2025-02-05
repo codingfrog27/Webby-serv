514cb7010c6b81a5c1c5f1435d5d20b54f47e03c
codingfrog27 <codingmerlin@gmail.com>
2025-02-05 18:01:37 +0100

    mini socket erorr change

--- sources/CGI/Socket.cpp
+++ sources/CGI/Socket.cpp
@@ -24,7 +24,7 @@ Socket::Socket(Config *config, const struct addrinfo *addressInfo)
 	{
 		if(_socketFd)
 			close(_socketFd);
-		std::cout << e.what() << '\n';
+		std::cout << e.what() << "\nserver setup failed, shutting down" << std::endl;
 		exit(1); //think we can exit cause  server socket creation kinda vital?
 	}
 	
