238b61b185735fe53e695d7b9d0fcd71fb3b2304
mstegema <mstegema@student.codam.nl>
2025-03-18 17:24:09 +0100

    Remove unused service links and delete obsolete CGI scripts

--- website/cgi-bin/upload.py
+++ website/cgi-bin/upload.py
@@ -65,7 +65,6 @@ body = f"""<!DOCTYPE html>
 				<li><a href="/index.html">Home</a></li>
 				<li><a href="/about.html">About</a></li>
 				<li><a href="/cgi-bin/list_files.py">Uploads</a></li>
-				<li><a href="/services.html">Services</a></li>
 				<li><a href="/contact.html">Contact</a></li>
 			</ul>
 		</nav>
